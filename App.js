15fff8e839e4cb4c84233d5c31f6d126734c9d4e
Rutwik sindkar <140788000+rutwik7389@users.noreply.github.com>
2025-02-24 16:46:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import HeroBanner from "./components/HeroBanner";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <HeroBanner />
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
